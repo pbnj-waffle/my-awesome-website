b8b013590f4d130b3cf4390cf36c0360d9097186
pbnj-waffle <shevtsova.zhenya@gmail.com>
2023-04-09 14:21:49 +0200

    update modified:   awesome-website.css modified:   awesome-website.html modified:   awesome-website.js

--- awesome-website.js
+++ awesome-website.js
@@ -1,1 +1,14 @@
-
+function createCanvas() {
+    // Get canvas width and height input values
+    var width = parseInt(document.getElementById("canvas-width").value);
+    var height = parseInt(document.getElementById("canvas-height").value);
+  
+    // Create canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.border = "1px solid black"; // Add border
+  
+    // Append canvas to the DOM
+    document.getElementById("canvas-container").appendChild(canvas);
+  }
