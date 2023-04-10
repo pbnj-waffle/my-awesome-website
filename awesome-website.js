0f8084d2a301aa7d021dab706fdc02ca9b534e8f
pbnj-waffle <shevtsova.zhenya@gmail.com>
2023-04-11 01:02:20 +0200

    update

--- awesome-website.js
+++ awesome-website.js
@@ -1,34 +1,105 @@
-function showCanvasSizeInputs() {
-    var canvasSizeInputs = document.getElementById("canvas-size-inputs");
-    if (canvasSizeInputs.classList.contains("hidden")) {
-      canvasSizeInputs.classList.remove("hidden");
-    } else {
-      canvasSizeInputs.classList.add("hidden");
+let canvas;
+let images = [];
+let handleSize = 10;
+let activeImage;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('canvasContainer');
+
+  const uploadImageButton = select('#uploadImage');
+  uploadImageButton.mousePressed(() => select('#fileInput').elt.click());
+
+  const fileInput = select('#fileInput');
+  fileInput.input(fileSelected);
+}
+
+function fileSelected() {
+  const newImage = loadImage(URL.createObjectURL(this.elt.files[0]), () => {
+    imageLoaded(newImage);
+  });
+}
+
+function imageLoaded(image) {
+  images.push({
+    img: image,
+    x: 0,
+    y: 0,
+    width: image.width,
+    height: image.height,
+    aspectRatio: image.width / image.height,
+    isDragging: false,
+    isResizing: false,
+  });
+}
+
+function mousePressed() {
+  for (let i = images.length - 1; i >= 0; i--) {
+    const imgData = images[i];
+
+    if (mouseX > imgData.x && mouseX < imgData.x + imgData.width && mouseY > imgData.y && mouseY < imgData.y + imgData.height) {
+      imgData.isDragging = true;
+      activeImage = imgData;
+      break;
+    }
+
+    if (mouseX > imgData.x + imgData.width - handleSize && mouseX < imgData.x + imgData.width + handleSize && mouseY > imgData.y + imgData.height - handleSize && mouseY < imgData.y + imgData.height + handleSize) {
+      imgData.isResizing = true;
+      activeImage = imgData;
+      break;
     }
   }
-  
-  function createCanvas() {
-    // Get canvas width and height input values
-    var width = parseInt(document.getElementById("canvas-width").value);
-    var height = parseInt(document.getElementById("canvas-height").value);
-  
-    // Create canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
-    canvas.style.border = "1px solid black"; // Add border
-  
-    // Set position to absolute and left/top properties to 0 to position canvas at the same spot as previous one
-    canvas.style.position = "absolute";
-    canvas.style.left = "100";
-    canvas.style.top = "100";
-    canvas.style.rigt = "100";
-    canvas.style.bottom = "100";
-  
-    // Append canvas to the DOM
-    document.getElementById("canvas-container").appendChild(canvas);
+}
+
+function mouseReleased() {
+  for (const imgData of images) {
+    imgData.isDragging = false;
+    imgData.isResizing = false;
   }
-  
-  
-  
-   
+}
+
+function draw() {
+  background(255);
+
+  for (const imgData of images) {
+    if (imgData.isDragging) {
+      imgData.x = mouseX - imgData.width / 2;
+      imgData.y = mouseY - imgData.height / 2;
+    } else if (imgData.isResizing) {
+      let newWidth = mouseX - imgData.x;
+      let newHeight = mouseY - imgData.y;
+
+      if (keyIsDown(SHIFT)) {
+        newHeight = newWidth / imgData.aspectRatio;
+      }
+
+      imgData.width = newWidth;
+      imgData.height = newHeight;
+    }
+
+    image(imgData.img, imgData.x, imgData.y, imgData.width, imgData.height);
+  }
+
+  if (activeImage) {
+    drawHandle(activeImage);
+
+    if (mouseX > activeImage.x && mouseX < activeImage.x + activeImage.width && mouseY > activeImage.y && mouseY < activeImage.y + activeImage.height) {
+      cursor(MOVE);
+    } else {
+      cursor(ARROW);
+    }
+  } else {
+    cursor(ARROW);
+  }
+}
+
+function drawHandle(imgData) {
+  fill(255, 0, 0);
+  noStroke();
+  rect(imgData.x + imgData.width - handleSize / 2, imgData.y + imgData.height - handleSize / 2, handleSize, handleSize);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
