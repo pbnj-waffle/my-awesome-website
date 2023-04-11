a4a32abfc7618e9509442dacd1d7ba2e47eb8e21
pbnj-waffle <shevtsova.zhenya@gmail.com>
2023-04-11 13:57:27 +0200

    update

--- image.js
+++ image.js
@@ -1,25 +1,7 @@
-let canvas;
-let images = [];
-let handleSize = 10;
-let activeImage;
 
 
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('canvasContainer');
 
-  const uploadImageButton = select('#uploadImage');
-  uploadImageButton.mousePressed(() => select('#fileInput').elt.click());
 
-  const fileInput = select('#fileInput');
-  fileInput.input(fileSelected);
-}
-
-function fileSelected() {
-  const newImage = loadImage(URL.createObjectURL(this.elt.files[0]), () => {
-    imageLoaded(newImage);
-  });
-}
 
 function imageLoaded(image) {
   images.push({
@@ -33,6 +15,7 @@ function imageLoaded(image) {
     isResizing: false,
   });
 }
+
 
 function mousePressed() {
   for (let i = images.length - 1; i >= 0; i--) {
@@ -59,40 +42,7 @@ function mouseReleased() {
   }
 }
 
-function draw() {
-  background(255);
 
-  for (const imgData of images) {
-    if (imgData.isDragging) {
-      imgData.x = mouseX - imgData.width / 2;
-      imgData.y = mouseY - imgData.height / 2;
-    } else if (imgData.isResizing) {
-      let newWidth = mouseX - imgData.x;
-      let newHeight = mouseY - imgData.y;
-
-      if (keyIsDown(SHIFT)) {
-        newHeight = newWidth / imgData.aspectRatio;
-      }
-
-      imgData.width = newWidth;
-      imgData.height = newHeight;
-    }
-
-    image(imgData.img, imgData.x, imgData.y, imgData.width, imgData.height);
-  }
-
-  if (activeImage) {
-    drawHandle(activeImage);
-
-    if (mouseX > activeImage.x && mouseX < activeImage.x + activeImage.width && mouseY > activeImage.y && mouseY < activeImage.y + activeImage.height) {
-      cursor(MOVE);
-    } else {
-      cursor(ARROW);
-    }
-  } else {
-    cursor(ARROW);
-  }
-}
 
 function drawHandle(imgData) {
   fill(255, 0, 0);
@@ -100,6 +50,3 @@ function drawHandle(imgData) {
   rect(imgData.x + imgData.width - handleSize / 2, imgData.y + imgData.height - handleSize / 2, handleSize, handleSize);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
--- text.js
+++ text.js
@@ -1,9 +1,6 @@
-let message = '';
 
 
-function draw() {
-  text(message, 25, 150);
-}
+
 
 function keyTyped() {
   message += key;
